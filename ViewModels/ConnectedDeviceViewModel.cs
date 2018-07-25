338c2eea82aec7013f595be68f2a61a1e1b92f40
Brad Nelson <bnelson@mtu.edu>
2018-07-24 21:28:40 -0400

    Create ConnectedDeviceViewModel

---
+++ ViewModels/ConnectedDeviceViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueEyes.ViewModels
+{
+    // I don't know yet...
+}
